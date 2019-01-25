3da8f5793b2fe3626ec85d63bdfc8f6a0d61bc9d
Faizal Triandi <faizaltriandi@student.telkomuniversity.ac.id>
2019-01-25 09:36:20 +0700

    Commit Pertama

---
+++ switch.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Switch</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<link href="https://fonts.googleapis.com/css?family=Righteous" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="assets/css/switch.css">
+	<link href="https://fonts.googleapis.com/css?family=Julius+Sans+One|Oleo+Script" rel="stylesheet">
+</head>
+<body id="baground">
+	<div id="votez">
+		<font>Votez.<br><p>Vote Bigest Simple One to Use</p></font>
+		
+	</div>
+
+		<!-- <table border="3" class="button">
+			<tr>
+				<td>
+					<a href="#">admin</a>
+				</td>
+			</tr>
+		</table> -->
+	<div id="button">
+		<div class="button1">
+			<a href="a-login.php">admin</a>
+		</div><br>
+		<div class="button1">
+			<a href="u-login.php" style="margin-left: 35%;">user.</a>
+		</div>
+	</div>
+
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</body>
+</html>
